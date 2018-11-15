8e424d7fe4f606b973ee5ed363d2ce9920d71272
Cristinamf <krismf98@gmail.com>
2018-11-15 13:48:11 +0100

    Primer intento

---
+++ src/MAIN.java
@@ -0,0 +1,120 @@
+import java.util.Scanner;
+
+public class MAIN {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner (System.in);
+		int pelo;
+		int ojos;
+		int orenar;
+		int boca;
+		String barbilla="   \"_____/";
+		
+		do {
+			System.out.println("1. WWWWWWWWW");
+			System.out.println("2. \\\\////");
+			System.out.println("3. |\"\"\"\"\"\"\"|");
+			System.out.println("4. |||||||||");
+			System.out.println("Elija pelo (numero entre 1 y 4):");
+			pelo=sc.nextInt();
+			if (pelo<1 || pelo>4) {
+				System.out.println("El numero introducido no es valido");
+			}
+		}while(pelo<1 || pelo>4);
+		
+		do {
+			System.out.println("1. |  O O  |");
+			System.out.println("2. |-(· ·)-|");
+			System.out.println("3. |-(o o)-|");
+			System.out.println("4. |  \"/  |");
+			System.out.println("Elija ojos (numero entre 1 y 4):");
+			ojos=sc.nextInt();
+			if (ojos<1 || ojos>4) {
+				System.out.println("El numero introducido no es valido");
+			}
+		}while(ojos<1 || ojos>4);	
+		
+		do {
+			System.out.println("1. @   J   @");						
+			System.out.println("2. {  \"   }");
+			System.out.println("3. [   j   ]");
+			System.out.println("4. <   -   >)");
+			System.out.println("Elija orejas y nariz (numero entre 1 y 4):");
+			orenar=sc.nextInt();
+			if (orenar<1 || orenar>4) {
+				System.out.println("El numero introducido no es valido");
+			}
+		}while(orenar<1 || orenar>4);	
+		
+		do {
+			System.out.println("1. |  ===  |");						
+			System.out.println("2. |   -   |");
+			System.out.println("3. |  ___  |");
+			System.out.println("4. |  ---  |");
+			System.out.println("Elija boca (numero entre 1 y 4):");
+			boca=sc.nextInt();
+			if (boca<1 || boca>4) {
+				System.out.println("El numero introducido no es valido");
+			}
+		}while(boca<1 || boca>4);
+		
+		switch(pelo) {
+			case 1:
+				System.out.println("1. WWWWWWWWW");
+				break;
+			case 2:
+				System.out.println("2. \\\\/////");
+				break;
+			case 3:
+				System.out.println("3. |\"\"\"\"\"\"\"|");
+				break;
+			case 4:
+				System.out.println("4. |||||||||");
+				break;	
+		}
+		switch(ojos) {
+			case 1:
+				System.out.println("1. |  O O  |");
+				break;
+			case 2:
+				System.out.println("2. |-(· ·)-|");
+				break;
+			case 3:
+				System.out.println("3. |-(o o)-|");
+				break;
+			case 4:
+				System.out.println("4. | \" /  |");
+				break;
+		}
+		switch(orenar) {
+			case 1:
+				System.out.println("1. @   J   @");						
+				break;
+			case 2:
+				System.out.println("2. {  \"   }");
+				break;
+			case 3:
+				System.out.println("3. [   j   ]");
+				break;
+			case 4:
+				System.out.println("4. <   -   >)");
+				break;
+		}
+		switch(boca) {
+			case 1:
+				System.out.println("1. |  ===  |");								
+				break;
+			case 2:
+				System.out.println("2. |   -   |");
+				break;
+			case 3:
+				System.out.println("3. |  ___  |");
+				break;
+			case 4:
+				System.out.println("4. |  ---  |");	
+				break;
+	}
+		System.out.println(barbilla);
+	}
+
+}
---
+++ src/MAIN2.java
@@ -0,0 +1,84 @@
+import java.util.Scanner;
+
+public class MAIN2 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner (System.in);
+		int pelo;
+		int ojos;
+		int orenar;
+		int boca;
+		String barbilla="   \"_______/";
+		
+		System.out.println("Pelo:           1. WWWWWWWWW 2. \\\\\\////// 3. |\"\"\"\"\"\"\"| 4. |||||||||");
+		System.out.println("Ojos:           1. |  O O  | 2. |-(· ·)-| 3. |-(o o)-| 4. |  \\\"/  |\"");
+		System.out.println("Orejas y nariz: 1. @   J   @ 2. {  \\\"   } 3. [   j   ] 4. <   -   >");
+		System.out.println("Boca:           1. |  ===  | 2. |   -   | 3. |  ___  | 4. |  ---  |");
+		System.out.println();
+		System.out.println("Elija pelo (numero entre 1 y 4):");
+		pelo=sc.nextInt();
+		System.out.println("Elija ojos (numero entre 1 y 4):");
+		ojos=sc.nextInt();
+		System.out.println("Elija orejas y nariz (numero entre 1 y 4):");
+		orenar=sc.nextInt();
+		System.out.println("Elija boca (numero entre 1 y 4):");
+		boca=sc.nextInt();
+		System.out.println();
+		
+		if (pelo<1 || pelo>4) {
+			System.out.println("El numero introducido no es valido");
+		}else {
+			if (pelo==1)
+				System.out.println("1. WWWWWWWWW");
+			else if(pelo==2)
+				System.out.println("2. \\\\////");
+			else if(pelo==3)
+				System.out.println("3. |\"\"\"\"\"\"\"|");
+			else if(pelo==4)
+				System.out.println("4. |||||||||");
+		}
+		
+		if (ojos<1 || ojos>4) {
+			System.out.println("El numero introducido no es valido");
+		}else {
+			if(ojos==1)
+				System.out.println("1. |  O O  |");
+			else if(ojos==2)
+			System.out.println("2. |-(· ·)-|");
+			else if(ojos==3)
+			System.out.println("3. |-(o o)-|");
+			else if(ojos==4)
+			System.out.println("4. |  \"/  |");
+		}
+		
+		if (orenar<1 || orenar>4) {
+			System.out.println("El numero introducido no es valido");
+		}else {
+			if(orenar==1)
+				System.out.println("1. @   J   @");	
+			else if(orenar==2)
+				System.out.println("2. {  \"   }");
+			else if(orenar==3)
+				System.out.println("3. [   j   ]");
+			else if(orenar==4)
+				System.out.println("4. <   -   >)");
+		}
+		
+		
+		if (boca<1 || boca>4) {
+			System.out.println("El numero introducido no es valido");
+		}else {
+			if (boca==1)
+				System.out.println("1. |  ===  |");		
+			else if(boca==2)
+				System.out.println("2. |   -   |");
+			else if(boca==3)
+				System.out.println("3. |  ___  |");
+			else if(boca==4)
+				System.out.println("4. |  ---  |");
+		}
+		System.out.println(barbilla);
+		
+	}
+}
+
+
